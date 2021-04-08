f418987065c190da4b5ddf287678411de21e62e5
Rohit Rajak <rohit.rajak@relyance.ai>
2021-04-08 20:35:30 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name=’opti-lib’,
+    packages=find_packages(include=[‘opti-lib’]),
+    version=’1.0.0',
+    description=’My first Python library’,
+    author=’Me’,
+    license=’MIT’,
+)
