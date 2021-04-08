5fa46c85f4462164e9202ea882c487849a367c7b
Rohit Rajak <rohit.rajak@relyance.ai>
2021-04-09 01:59:17 +0530

    setuptools added

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 setup(
-    name=’opti-lib’,
-    packages=find_packages(include=[‘opti-lib’]),
-    version=’1.0.0',
-    description=’My first Python library’,
-    author=’Me’,
-    license=’MIT’,
+    name="opti-lib",
+    packages=find_packages(),
+    maintainer_email="rohitrajak1907@gmail.com",
+    version="1.0.0",
+    description="Python library to Optimize the cost of Transportation in U/G mines",
+    author="Rohit Rajak",
+    license="MIT",
+    python_requires='>=3.8'
 )
